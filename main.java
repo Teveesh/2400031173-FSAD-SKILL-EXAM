50af2bb85adb273893eea5160eb4233d0248dadf
Teveesh <teveesht@gmail.com>
2026-03-11 10:50:57 +0530

    Delete main.java

--- main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello FSAD Skill Experiment");
-        System.out.println("Git Version Control Demo");
-    }
-}
